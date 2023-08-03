37766df1710cd17bfde13a85f5c491bc27b7d88d
mabattal <mehmetalibattal@gmail.com>
2023-08-03 15:44:39 +0300

    Not Entity'si oluşturulması

---
+++ EntityLayer/Concrete/Not.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Not
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Konu { get; set; }
+        public string Metin { get; set; }
+        public DateTime Tarih { get; set; } = DateTime.Now;
+        public bool NotSil { get; set; } = false;
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (string.IsNullOrEmpty(Konu))
+                yield return new ValidationResult("Konu boş olamaz.", new[] { nameof(Konu) });
+
+            if (string.IsNullOrEmpty(Metin))
+                yield return new ValidationResult("Metin boş olamaz.", new[] { nameof(Metin) });
+        }
+    }
+
+
+
+
+
+
+}
