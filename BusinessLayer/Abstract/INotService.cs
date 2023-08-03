2a797bdca0ae69d9746117474f0030a192cf1953
mabattal <mehmetalibattal@gmail.com>
2023-08-03 17:50:43 +0300

    Business katmanı servise ve manager deneme

---
+++ BusinessLayer/Abstract/INotService.cs
@@ -0,0 +1,17 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface INotService
+    {
+        void NotInsert(Not not);
+        void NotDelete(Not not);
+        List<Not> GetListAll();
+        Not GetById(int id);
+    }
+}
---
+++ BusinessLayer/Concrete/NotManager.cs
@@ -0,0 +1,33 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class NotManager : INotService
+    {
+        public Not GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Not> GetListAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NotDelete(Not not)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NotInsert(Not not)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
