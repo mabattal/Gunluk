58354be47311bd29001e9a8e982649ee550aa1ae
mabattal <mehmetalibattal@gmail.com>
2023-08-09 13:35:25 +0300

    Proje sevisyesinde Authorize

--- Gunluk/Controllers/RegisterController.cs
+++ Gunluk/Controllers/RegisterController.cs
@@ -3,10 +3,12 @@ using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
 using FluentValidation.Results;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Gunluk.Controllers
 {
+    [AllowAnonymous]
     public class RegisterController : Controller
     {
         YazarManager yazarManager = new YazarManager(new EfYazarRepository());
--- Gunluk/Program.cs
+++ Gunluk/Program.cs
@@ -1,7 +1,18 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddMvc(config =>
+{
+    var policy = new AuthorizationPolicyBuilder()
+    .RequireAuthenticatedUser()
+    .Build();
+    config.Filters.Add(new AuthorizeFilter(policy));
+});
 
 var app = builder.Build();
 
