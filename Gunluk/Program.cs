2a1f2587f525de33792bec917bba945a8b8bb670
mabattal <mehmetalibattal@gmail.com>
2023-08-09 14:27:35 +0300

    Authorize ile Return Login Url

--- Gunluk/Program.cs
+++ Gunluk/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Authorization;
 
@@ -15,6 +16,14 @@ builder.Services.AddMvc(config =>
     .Build();
     config.Filters.Add(new AuthorizeFilter(policy));
 });
+
+builder.Services.AddMvc();
+builder.Services.AddAuthentication(
+    CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(x =>
+    {
+        x.LoginPath = "/Login/Index";
+    });
 
 var app = builder.Build();
 
