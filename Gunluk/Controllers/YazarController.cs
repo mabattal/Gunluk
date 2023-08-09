015f1e9f8a00c1575057d77c318005427e382023
mabattal <mehmetalibattal@gmail.com>
2023-08-09 18:30:44 +0300

    Yazar Düzenle ekranı yapıldı

--- Gunluk/Controllers/YazarController.cs
+++ Gunluk/Controllers/YazarController.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Gunluk.Controllers
@@ -12,8 +14,35 @@ namespace Gunluk.Controllers
         {
             List<Yazar> yazarListesi = yazarManager.GetListAll();
             var values = yazarListesi.Where(yazar => yazar.YazarSil == false).ToList();
-           
+
             return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult YazarDuzenle()
+        {
+            var yazarValues = yazarManager.TGetById(1);
+            return View(yazarValues);
+        }
+
+        [HttpPost]
+        public IActionResult YazarDuzenle(Yazar yazar)
+        {
+            YazarValidator yazarValidator = new YazarValidator();
+            ValidationResult results = yazarValidator.Validate(yazar);
+            if (results.IsValid)
+            {
+                yazarManager.TUpdate(yazar);
+                return RedirectToAction("Index", "Not");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                return View();
+            }
         }
     }
 }
