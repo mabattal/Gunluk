959ed8de9f2392db3c150b864e6893a9a7212344
mabattal <mehmetalibattal@gmail.com>
2023-08-10 16:18:09 +0300

    swagger ve api get list

---
+++ ApiLayer/Controllers/DefaultController.cs
@@ -0,0 +1,19 @@
+﻿using ApiLayer.DataAccessLayer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiLayer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult CalisanListe()
+        {
+            using var context = new Context();
+            var values = context.Calisans.ToList();
+            return Ok(values);
+        }
+    }
+}
---
+++ ApiLayer/DataAccessLayer/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ApiLayer.DataAccessLayer
+{
+    public class Context :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-73I52K7; database=CoreGunlukApi; integrated security=True; TrustServerCertificate=True;");
+        }
+
+        public DbSet<Calisan> Calisans { get; set; }
+    }
+}
