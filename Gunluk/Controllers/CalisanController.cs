fa14d584376b56ee410eedc5ea702e16b27d5b10
mabattal <mehmetalibattal@gmail.com>
2023-08-11 14:57:24 +0300

    api - proje üzerinden ekleme

---
+++ Gunluk/Controllers/CalisanController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Gunluk.Controllers
+{
+    public class CalisanController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var httpClient = new HttpClient();
+            var responseMessage = await httpClient.GetAsync("https://localhost:7183/api/Default");
+            var jsonString = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<Class1>>(jsonString);
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CalisanEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CalisanEkle(Class1 class1)
+        {
+            var httpClient = new HttpClient();
+            var jsonCalisan = JsonConvert.SerializeObject(class1);
+            StringContent content = new StringContent(jsonCalisan, Encoding.UTF8, "application/json");
+            var responseMessage = await httpClient.PostAsync("https://localhost:7183/api/Default", content);
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(jsonCalisan);
+        }
+    }
+
+    public class Class1
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+    }
+}
